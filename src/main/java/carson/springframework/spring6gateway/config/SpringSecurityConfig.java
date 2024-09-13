337bfeb382ec1077c810494a99e513713d6a9a3e
Guanhong Liu <guanhongliu45@gmail.com>
2024-09-13 09:43:40 -0400

    bugs with docker!!

--- src/main/java/carson/springframework/spring6gateway/config/SpringSecurityConfig.java
+++ src/main/java/carson/springframework/spring6gateway/config/SpringSecurityConfig.java
@@ -1,8 +1,9 @@
 package carson.springframework.spring6gateway.config;
 
-
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -14,9 +15,20 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 public class SpringSecurityConfig {
 
     @Bean
+    @Order(1)
+    public SecurityWebFilterChain actuatorSecurityFilterChain(ServerHttpSecurity http) throws Exception {
+        http.securityMatcher(EndpointRequest.toAnyEndpoint())
+                .authorizeExchange(authorize -> authorize.anyExchange().permitAll());
+
+        return http.build();
+    }
+
+    @Bean
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http){
 
-        http.authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
+        http.authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec
+                .pathMatchers("/oauth2/**","oauth2/token").permitAll()
+                .anyExchange().authenticated())
                 .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()))
                 .csrf(ServerHttpSecurity.CsrfSpec::disable);
         return http.build();
